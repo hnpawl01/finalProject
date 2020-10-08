f4d5d3a7624635fcbe37c0236bab66c522d5a733
Hannah Pawley <pawely10201@yahoo.com>
2020-10-08 02:11:21 -0400

    done may

--- js/calltoa.js
+++ js/calltoa.js
@@ -1,4 +1,4 @@
-var eduPromises = d3.csv("../education.csv");
+var callPromises = d3.csv("../calltoas.csv");
 
 //----------drawingFinalvsHW-------------
 	var drawPlot = function (education,target,xScale,yScale)
@@ -9,14 +9,17 @@ var eduPromises = d3.csv("../education.csv");
 		.enter()
 		.append("circle")
 		.attr("cx", function(makeup)
-			 {
-			return xScale(makeup.totalcap)
-			})
+	 	{
+			return xScale(makeup.monPercap)
+		})
 		.attr("cy", function(makeup)
-			 {
+		{
 			return yScale(makeup.math)
-			})
-		.attr("r",2) 
+		})
+		.attr("r",function(makeup)
+		{
+			return ((100-makeup.perenrolled))
+		}) 
 		
 		.on("mouseover", function(makeup)
 		   {
@@ -29,9 +32,11 @@ var eduPromises = d3.csv("../education.csv");
 			d3.select("#scatter")
 			.text(makeup.country)
 			d3.select("#money")
-			.text(makeup.totalcap)
+			.text(makeup.monPercap)
 			d3.select("#mathscores")
 			.text(makeup.math)
+			d3.select("#out")
+			.text(makeup.totalout)
 		})}
 var drawAxis = function(graph,margins,xScale,yScale)
 	{
@@ -47,27 +52,53 @@ var drawAxis = function(graph,margins,xScale,yScale)
 	.attr("transform", "translate (" + margins.left + "," + (margins.top) + ")")
 	.call(yAxis) 
 	}
+var drawLabels = function(graph,margins)
+	{
+		var labels = d3.select("svg")
+        .append("g")
+        .classed("labels",true)
+	   
+	   labels.append("text")
+		.classed("title", true)
+		.text("Math Scores vs Money")
+        .attr("text-anchor","middle")
+		.attr("x", margins.left + graph.width/2)
+		.attr("y", margins.top + (10))
+	
+	labels.append("text")
+		.classed("label", true)
+		.text("Money per Capita")
+        .attr("text-anchor","middle")
+		.attr("x", margins.left + graph.width/2)
+		.attr("y", margins.top + (graph.height) +(30
+												 ))
+	
+	labels.append("text")
+		.classed("label", true)
+		.text("Math Test Scores")
+        .attr("text-anchor","middle")
+		.attr( "transform" , "rotate(90) translate (" +(graph.height/2)  + ",-3)")
+	}
 	
 //---------scale---------
 var initgraph = function (education)
 {
 	console.log("insideinit",education)
 	var screen = {width:700, height:500}
-	var margins = {left:30, right:20, top:20, bottom:20}
+	var margins = {left:60, right:20, top:20, bottom:40}
 	var graph = 
 		{
 			width: screen.width - margins.left - margins.right, 
 			height: screen.height - margins.top - margins.bottom
 		}
-	
-    d3.select("svg")
-	.attr("width",screen.width)
-	.attr("height",screen.height)
-	
 	var target = d3.select("svg")
 	.append("g")
 	.attr("id", "#graph")
 	.attr("transform", "translate(" + margins.left + ", " + margins.top + ")")
+	
+    d3.select("svg")
+	.attr("width",screen.width)
+	.attr("height",screen.height)
 	
 	d3.select("#graph")
 	.attr("width", screen.width)
@@ -80,7 +111,7 @@ var initgraph = function (education)
 					.range([graph.height,0])
 	drawAxis(graph,margins,xScale,yScale)
 	drawPlot(education,target,xScale,yScale);
-		
+	drawLabels(graph,margins)	
 }
 
 //--------promise-----------
@@ -94,4 +125,4 @@ var failureFcn = function(error)
 	console.log("error", error)
 
 }
-eduPromises.then(successFcn, failureFcn)
+callPromises.then(successFcn, failureFcn)
